0bb66c4414f0df073fd37a891815b445497df641
Wisdom Asigbey <113071078+WisdomAsigbey@users.noreply.github.com>
2025-07-02 19:25:48 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const btn = document.querySelector(".btn");
+const url = "./api/people.json";
+
+btn.addEventListener("click", () => {
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+      displayItems(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+const displayItems = (items) => {
+  const displayData = items
+    .map((item) => {
+      const { name } = item;
+      return `<p>${name}</p>`;
+    })
+    .join("");
+
+  const element = document.createElement("div");
+  element.innerHTML = displayData;
+  document.body.appendChild(element);
+};
